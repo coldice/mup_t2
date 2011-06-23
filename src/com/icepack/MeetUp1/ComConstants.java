57c46c8ab264a92071c0f50a7f3c996e2ba7f74d
algorim <x33dness@gmx.de>
2011-06-23 05:10:24 +0200

    Change-Id: If68810e69a85675174b5711643593dc1020c3bbd

--- src/com/icepack/MeetUp1/ComConstants.java
+++ src/com/icepack/MeetUp1/ComConstants.java
@@ -8,10 +8,15 @@ package com.icepack.MeetUp1;
  * 
  */
 public class ComConstants {
-	public static final byte ADD_DEVICE = 1;
-	public static final byte DEL_DEVICE = 2;
-	public static final byte GET_LOC = 3;
-	public static final byte SET_LOC = 4;
-	public static final byte ERROR = 5;
-	public static final byte NOERROR = 6;
+	public static final byte ROOM_LOGIN= 1;
+	public static final byte ROOM_GETUSERS = 2;
+	public static final byte ROOM_LOGOUT = 3;
+	public static final byte ROOM_GET = 4;
+	public static final byte REGISTER = 5;
+	public static final byte UNREGISTER = 6;
+	public static final byte LOC_GET = 9;
+	public static final byte LOC_SET = 10;
+	public static final byte ERROR = 7;
+	public static final byte NOERROR = 8;
 }
+
