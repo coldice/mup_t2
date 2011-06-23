b54c3ac9a6eae293aaff26298ce5ee04481901d0
coldice <dev.coldice@gmail.com>
2011-06-23 04:44:21 +0200

    updated common

---
+++ src/com/icepack/MeetUp1/common/MURoom.java
@@ -0,0 +1,12 @@
+package com.icepack.MeetUp1.common;
+
+public class MURoom {
+	public int id;
+	public String name;
+	
+	public MURoom(int id, String name)
+	{
+		this.id = id;
+		this.name = name;
+	}
+}
---
+++ src/com/icepack/MeetUp1/common/MUUser.java
@@ -0,0 +1,11 @@
+package com.icepack.MeetUp1.common;
+
+public class MUUser {
+	public int id;
+	public String username;
+	
+	public MUUser(int id, String username) {
+		this.id = id;
+		this.username = username;
+	}
+}
